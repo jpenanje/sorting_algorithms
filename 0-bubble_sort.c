dff403d74c3127c53071fcd80dda9437c3ad8fdf
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-07-19 17:23:07 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -29,7 +29,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 				swap = true;
-				print_array(array);
+				print_array(array, size);
 			}
 		}
 		i++;
