169c319bc3b18ee90bea25abef7c2ecfbebd5fbc
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-07-20 01:20:55 +0100

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,23 +10,35 @@
 void selection_sort(int *array, size_t size)
 {
 
-	size_t i, j, Min;
+size_t i, j, jMin;
 
-	for (i = 0; i <= size; i++)
+for (i = 0; i <= size; i++)
+{
+	jMin = i; /*consider the first element as min*/
+	for (j = i + 1; j < size; j++)
 	{
-		Min = i; /*consider the first element as min*/
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[Min])
-				Min = j;
-		}
-		if (Min != i)
-		{
-			tmp = array[i];
-			array[i] = array[Min];
-			array[Min] = tmp;
-			print_array(array, size);
-		}
+		if (array[j] < array[jMin])
+			jMin = j;
 	}
+	if (jMin != i)
+	{
+		swap_int(array, i, jMin);
+		print_array(array, size);
+	}
+}
 
+}
+
+/**
+ * swap_int - swap variable values
+ * @array: array to use
+ * @a: index 1
+ * @b: index 2
+*/
+void swap_int(int *array, size_t a, size_t b)
+{
+int tmp;
+tmp = array[a];
+array[a] = array[b];
+array[b] = tmp;
 }
