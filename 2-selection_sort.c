1ff85418e60a2e436984692153fecfb4456dd6fe
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-07-20 01:17:30 +0100

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,17 +9,16 @@
 
 void selection_sort(int *array, size_t size)
 {
-	int i, j, tmp;
+	int i, j, tmp, min_index;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i <= size; i++)
 	{
-		int min_index = i;
+		min_index = i;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
 				min_index = j;
 		}
-
 		if (min_index != i)
 		{
 			tmp = array[i];
